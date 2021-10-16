d712b52d29ec2acf1b77f1ba0ad7af126871ecf0
Geovane Ferreira <geovanevinicius89@gmail.com>
2021-10-16 09:48:26 -0300

    Example FreeRTOS
    - Running 2 Threads

--- POC02-freertos/main/main.c
+++ POC02-freertos/main/main.c
@@ -11,7 +11,50 @@
 #include "freertos/task.h"
 #include "esp_system.h"
 #include "esp_spi_flash.h"
+#include "driver/gpio.h"
+#include "esp_log.h"
 
+#define BLINK_GPIO 2
+
+static const char *TAG = "ExampleRTOS";
+static uint8_t s_led_state = 0;
+
+static void configure_led(void)
+{
+    ESP_LOGI(TAG, "Configured to blink GPIO LED!");
+    gpio_reset_pin(BLINK_GPIO);
+    /* Set the GPIO as a push/pull output */
+    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+}
+
+static void blink_led(void)
+{
+    /* Set the GPIO level according to the state (LOW or HIGH)*/
+    gpio_set_level(BLINK_GPIO, s_led_state);
+}
+
+static void task_blinkled(void *aContext)
+{
+    configure_led();
+    while (1) {
+        ESP_LOGI(TAG, "Turning the LED %s!", s_led_state == true ? "ON" : "OFF");
+        blink_led();
+        /* Toggle the LED state */
+        s_led_state = !s_led_state;
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
+
+static void task_print(void *aContext)
+{
+    uint16_t counter = 0;
+    while (1) {
+        ESP_LOGI(TAG, "Running task print %d", counter++);
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
+        if(counter > 1000)
+            counter = 0;
+    }
+}
 
 void app_main(void)
 {
@@ -30,11 +73,13 @@ void app_main(void)
     printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
             (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
 
-    for (int i = 10; i >= 0; i--) {
-        printf("Restarting in %d seconds...\n", i);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
-    printf("Restarting now.\n");
-    fflush(stdout);
-    esp_restart();
+    xTaskCreate(task_blinkled, "blink_led", 10240, xTaskGetCurrentTaskHandle(), 5, NULL);
+    xTaskCreate(task_print, "prints", 10240, xTaskGetCurrentTaskHandle(), 3, NULL);
+    // for (int i = 10; i >= 0; i--) {
+    //     printf("Restarting in %d seconds...\n", i);
+    //     vTaskDelay(1000 / portTICK_PERIOD_MS);
+    // }
+    // printf("Restarting now.\n");
+    // fflush(stdout);
+    // esp_restart();
 }
